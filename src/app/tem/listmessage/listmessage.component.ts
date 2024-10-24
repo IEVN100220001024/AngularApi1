37e9f6e13b5b8f6b4812ed7e66c6a7261731294c
Juan Emiliano Saldaña Hernandez <soyemiliano25@hotmail.com>
2024-10-23 21:08:31 -0600

    primer comit api

---
+++ src/app/tem/listmessage/listmessage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listmessage',
+  standalone: true,
+  imports: [],
+  templateUrl: './listmessage.component.html',
+  styles: ``
+})
+export class ListmessageComponent {
+
+}
